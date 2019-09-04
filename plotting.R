3235571219092faf0e140478df99ac2b5c429dfc
Fabio Niephaus <code@fniephaus.com>
2019-09-04 12:03:55 +0200

    Render plots at higher resolution

--- plotting.R
+++ plotting.R
@@ -1,6 +1,9 @@
 library(ggplot2)
 library(dplyr)
 
+svgWidth <- 12
+svgHeight <- svgWidth / 2
+
 if (exists('import')) {
   statsObject <- import('stats')
 }
@@ -31,9 +34,8 @@ load_data_file <- function (file) {
   bench
 }
 
-example_plot <- function (contents) {
-  svg()
-  plot <- ggplot(mpg, aes(x = displ, y = hwy)) + geom_point()
+toSVG <- function (plot) {
+  svg(width = svgWidth, height = svgHeight)
   print(plot)
   return(svg.off())
 }
@@ -41,20 +43,15 @@ example_plot <- function (contents) {
 benchmark_plot <- function (file1, benchName, startIteration) {
   data <- load_data_file(file1)
   data <- subset(data, Benchmark == benchName)
-  if (startIteration > 0) {
-    data <- tail(data, -startIteration)
-  }
+  data <- data %>% filter(iteration > startIteration)
   if (exists('statsObject')) {
     statsObject$run1$max <- max(data$Value)
     statsObject$run1$min <- min(data$Value)
     statsObject$run1$mean <- mean(data$Value)
-    statsObject$run1$median <- median(data1$Value)
-    statsObject$run1$geomean <- exp(mean(log(data1$Value)))
+    statsObject$run1$median <- median(data$Value)
+    statsObject$run1$geomean <- exp(mean(log(data$Value)))
   }
   
-  # print(data)
-  
-  svg(width = 8, height = 4)
   plot <- ggplot(data, aes(x=iteration, y=Value, group=Benchmark)) + geom_point(size=1)
   #plot <- plot + geom_point(aes(color=Benchmark))
   plot <- plot + xlab('') + ylab(data$Unit)
@@ -62,19 +59,16 @@ benchmark_plot <- function (file1, benchName, startIteration) {
   #plot <- plot + geom_smooth()
   #plot <- plot + scale_x_continuous(breaks = seq(0, max(data$iteration), 100))
   plot <- plot + geom_vline(xintercept = c(50, 100, 150, 200, 250), linetype = "longdash", colour = "#cccccc")
-  print(plot)
-  return(svg.off())
+  return(toSVG(plot))
 }
 
 benchmark_diff_plot <- function (file1, file2, benchName, startIteration) {
   data1 <- load_data_file(file1)
   data1 <- subset(data1, Benchmark == benchName)
+  data1 <- data1 %>% filter(iteration > startIteration)
   data2 <- load_data_file(file2)
   data2 <- subset(data2, Benchmark == benchName)
-  if (startIteration > 0) {
-    data1 <- tail(data1, -startIteration)
-    data2 <- tail(data2, -startIteration)
-  }
+  data2 <- data2 %>% filter(iteration > startIteration)
   if (exists('statsObject')) {
     statsObject$run1$max <- max(data1$Value)
     statsObject$run1$min <- min(data1$Value)
@@ -89,7 +83,6 @@ benchmark_diff_plot <- function (file1, file2, benchName, startIteration) {
   }
   # print(data1)
   
-  svg(width = 8, height = 4)
   plot <- ggplot() + geom_point(data=data1, aes(x=iteration, y=Value), shape=16, size=0.5)
   plot <- plot + geom_point(data=data2, aes(x=iteration, y=Value), shape=17, size=0.5, color='red')
   plot <- plot + geom_point(size=1)
@@ -99,22 +92,18 @@ benchmark_diff_plot <- function (file1, file2, benchName, startIteration) {
   plot <- plot + geom_smooth(method = 'loess')
   #plot <- plot + scale_x_continuous(breaks = seq(0, max(data$iteration), 100))
   plot <- plot + geom_vline(xintercept = c(50, 100, 150, 200, 250), linetype = "longdash", colour = "#cccccc")
-  print(plot)
-  return(svg.off())
+  return(toSVG(plot))
 }
 
 summary_plot <- function (file, startIteration) {
   data <- load_data_file(file)
   data <- data %>% group_by(Benchmark) %>% filter(iteration > startIteration)
-  #data <- tail(data, -100)
-  svg(width = 8, height = 4)
   plot <- ggplot(data, aes(x=Benchmark, y=Value))
   plot <- plot + geom_boxplot(lwd = 0.4, show.legend = FALSE, outlier.size=0.5, outlier.colour = "#ff0000")
   plot <- plot + xlab('') + ylab(data$Unit)
-  plot <- plot + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  plot <- plot + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0))
   #plot <- plot + scale_x_continuous(breaks = seq(0, max(data$iteration), 100))
-  print(plot)
-  return(svg.off())
+  return(toSVG(plot))
 }
 
 summary_diff_plot <- function (commit1, file1, commit2, file2, startIteration) {
@@ -126,12 +115,9 @@ summary_diff_plot <- function (commit1, file1, commit2, file2, startIteration) {
   data2$commit <- commit2
   data <- rbind(data1, data2)
   # print(data)
-  #data <- tail(data, -100)
-  svg(width = 8, height = 4)
   plot <- ggplot(data, aes(x=commit, y=Value, group=commit)) + facet_wrap(~Benchmark, scale="free")
   plot <- plot + geom_boxplot(lwd = 0.4, show.legend = FALSE, outlier.size=0.5, outlier.colour = "#ff0000")
   plot <- plot + xlab('') + ylab(data$Unit)
   #plot <- plot + scale_x_continuous(breaks = seq(0, max(data$iteration), 100))
-  print(plot)
-  return(svg.off())
+  return(toSVG(plot))
 }
